d574bbbec2370f294cf6ca5cb2e88517d57b441c
fwg-dev <fwgicheh@gmail.com>
2021-10-06 16:27:53 -0700

    add useState hook, functions that implement and update the todo state

--- src/App.js
+++ src/App.js
@@ -4,21 +4,37 @@ import './App.css';
 
 function App() {
 
-  const [todos, setTodos] = useState(['Build a todo app', 'Build a todo app', 'Build a todo app']); 
+  const [todos, setTodos] = useState(['Build a todo app', 'Build a static login page', 'Brush up on React fundamentals', 'make a dental appointment']); 
+  const [input, setInput] = useState(''); 
+  console.log(input); 
 
+  const addTodo = (event => {
+    //happens when we click the button 
+    event.preventDefault(); //Prevents form from refresshing everytime we click submit 
+    console.log('the button is working')
+    setTodos([...todos, input]);
+    setInput(''); //clears input, sets it back to blank 
+
+  })
 
   return (
     <div className="App">
      <h1>ToDo App </h1> 
 
-<input/>
-<button>Add Task</button>
+<input 
+value={input} 
+  onChange={event =>setInput(event.target.value)}
+/>
+<button type="submit"
+onClick={addTodo}
+>Add Task</button>
 <ul>
 
-  <li></li>
-  <li></li>
-  <li></li>
-  <li></li>
+
+{todos.map( todo  => ( 
+  <li>{todo}</li>
+))}
+  
 </ul>
     </div>
   );
