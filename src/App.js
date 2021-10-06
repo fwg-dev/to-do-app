3d4bc8bdb8ac01ff425c657b2a601c31c321dd3d
fwg-dev <fwgicheh@gmail.com>
2021-10-06 13:30:22 -0700

    import useState hook, and build app skeleton using basic div, input and li elements

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
-import 'react' from React; 
+import React, {useState} from 'react';
 import './App.css';
 
+
 function App() {
+
+  const [todos, setTodos] = useState(['Build a todo app', 'Build a todo app', 'Build a todo app']); 
+
+
   return (
     <div className="App">
-      Hello from App.js
+     <h1>ToDo App </h1> 
+
+<input/>
+<button>Add Task</button>
+<ul>
+
+  <li></li>
+  <li></li>
+  <li></li>
+  <li></li>
+</ul>
     </div>
   );
 }
