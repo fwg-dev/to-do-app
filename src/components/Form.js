c5fb9359e6e671bbf76870322fab0f3b740a347a
fwg-dev <fwgicheh@gmail.com>
2021-10-07 09:09:04 -0700

    added forms, useState hooks, added onChange and onClick functions to the button and input. Import form and button styles from material ui

---
+++ src/components/Form.js
@@ -0,0 +1,56 @@
+import React, { useState, useEffect } from "react";
+import { Button, FormControl, InputLabel, Input } from "@mui/material";
+
+import Todos from "./Todos";
+
+function Form({todos, setTodos}) {
+
+    const [input, setInput] = useState("");
+    // console.log(input);
+  
+    const addTodo = (event) => {
+      //happens when we click the button
+      event.preventDefault(); //Prevents form from refresshing everytime we click submit
+      setTodos([...todos, input]);
+      setInput(""); //clears input, sets it back to blank
+    };
+
+    // useEffect(() => {
+    //   effect
+    //   return () => {
+    //     cleanup
+    //   }
+    // }, [input])
+  
+  return (
+    <div>
+    <FormControl>
+        <InputLabel>Write your task here</InputLabel>
+        <Input
+          value={input}
+          onChange={(event) => setInput(event.target.value)}
+        />
+      </FormControl>
+
+      <Button
+        disabled={!input}
+        variant="contained"
+        type="submit"
+        onClick={addTodo}
+      >
+        Add todo
+      </Button>
+
+      <ul>
+        {todos.map((todo) => (
+          <Todos todos={todo} />
+          //<li>{todo}</li>
+        ))}
+      </ul>
+
+  </div>
+  );
+}
+
+
+export default Form;
