1a5c97e9fa905bcc01863c5cb38b5f31e70ff175
fwg-dev <fwgicheh@gmail.com>
2021-10-07 09:14:45 -0700

    added files that will hold notes, and individal todo items

---
+++ notes.md
@@ -0,0 +1,9 @@
+
+{/* // <button  type="submit" onClick={addTodo} >Add Task</button>  */}
+
+      {/* <ul>
+        {todos.map((todo) => (
+          <Todos todos={todo} />
+          //<li>{todo}</li>
+        ))}
+      </ul> */}
---
+++ src/components/Todo.js
@@ -0,0 +1,20 @@
+//holds a single row of to do
+//here we can implement CRUD for one sinlge todo
+
+import React from 'react'
+// import { List, ListItem, ListItemText, IconButton,  } from '@mui/material';
+// // import DeleteIcon from '@mui/icons-material/Delete';
+
+function Todo() {
+  return (
+    <div>
+       <li> Hello there</li>
+       <button>X</button>
+       <button>+</button>
+      
+    </div>
+  )
+}
+
+export default Todo; 
+
