59daa2334c86f51b43281043e058952e2163da71
fwg-dev <fwgicheh@gmail.com>
2021-10-06 17:22:15 -0700

    imported Button, Input, FormControl, InputLabel from materia ui and injected the styles in their respective elements

--- src/App.js
+++ src/App.js
@@ -1,41 +1,53 @@
-import React, {useState} from 'react';
-import './App.css';
-
+import { Button, FormControl, InputLabel, Input } from "@mui/material";
+import React, { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [todos, setTodos] = useState([
+    "Build a todo app",
+    "Build a static login page",
+    "Brush up on React fundamentals",
+    "make a dental appointment",
+  ]);
+  const [input, setInput] = useState("");
+  console.log(input);
 
-  const [todos, setTodos] = useState(['Build a todo app', 'Build a static login page', 'Brush up on React fundamentals', 'make a dental appointment']); 
-  const [input, setInput] = useState(''); 
-  console.log(input); 
-
-  const addTodo = (event => {
-    //happens when we click the button 
-    event.preventDefault(); //Prevents form from refresshing everytime we click submit 
-    console.log('the button is working')
+  const addTodo = (event) => {
+    //happens when we click the button
+    event.preventDefault(); //Prevents form from refresshing everytime we click submit
+    console.log("the button is working");
     setTodos([...todos, input]);
-    setInput(''); //clears input, sets it back to blank 
-
-  })
+    setInput(""); //clears input, sets it back to blank
+  };
 
   return (
     <div className="App">
-     <h1>ToDo App </h1> 
+      <h1>ToDo App </h1>
 
-<input 
-value={input} 
-  onChange={event =>setInput(event.target.value)}
-/>
-<button type="submit"
-onClick={addTodo}
->Add Task</button>
-<ul>
+      <FormControl>
+        <InputLabel>Write your task here</InputLabel>
+        <Input
+          value={input}
+          onChange={(event) => setInput(event.target.value)}
+        />
+      </FormControl>
 
+      <Button
+        disabled={!input}
+        variant="contained"
+        type="submit"
+        onClick={addTodo}
+      >
+        Add Task
+      </Button>
 
-{todos.map( todo  => ( 
-  <li>{todo}</li>
-))}
-  
-</ul>
+      {/* <button  type="submit" onClick={addTodo} >Add Task</button> */}
+
+      <ul>
+        {todos.map((todo) => (
+          <li>{todo}</li>
+        ))}
+      </ul>
     </div>
   );
 }
