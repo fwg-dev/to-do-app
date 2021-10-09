487164f3baa8daa4bd9ffe42a975a0f9726d0c1f
fwg-dev <fwgicheh@gmail.com>
2021-10-09 15:15:54 -0700

    add a completed class that will strike through line on the text when the checkbox is clicked. Add a completeTodo function and a deleteTodo function.

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -2,17 +2,41 @@
 //here we can implement CRUD for one sinlge todo
 
 import React from "react";
+import "./Todo.css";
+
 import { List, ListItem, ListItemText, IconButton, Checkbox } from "@mui/material";
 import DeleteIcon from "@mui/icons-material/Delete";
 
-function Todo({text}) {
+function Todo({text, id, todo, todos, setTodos}) {
+
+  //CRUD events 
+  const deleteTodo = () => {
+    setTodos(todos.filter((el) => el.id !== todo.id)); 
+    alert("This Task has been deleted!")
+    
+  };
+
+  const completeTodo = () => {
+    setTodos(todos.map(item => {
+      if(item.id === todo.id){
+        return {
+          ...item, 
+          completed: !item.completed,
+        };
+      }
+        return item; 
+      })
+    );
+
+ }
+
   return (
     <div>
-      <List>
-      <ListItem primary={text} secondary="deadline">
-     {text}
-        <Checkbox />
-           <IconButton edge="end" aria-label="delete">
+      <List className={`todo-item ${todo.completed  ? "completed" : '' }`}>
+      <ListItem >
+         {text}
+        <Checkbox  onChange={completeTodo }  />
+           <IconButton edge="end" aria-label="delete" onClick={deleteTodo } >
                <DeleteIcon />
                </IconButton>
         </ListItem>
