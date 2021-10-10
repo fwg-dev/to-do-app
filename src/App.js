5bff84f35bd52880ff1bbe7f357c088c29440195
fwg-dev <fwgicheh@gmail.com>
2021-10-09 17:23:16 -0700

    set up a filteredTodo useState and useEffect that would enable user to toggle between completed, not completed and all todos. Created a filter function with conditional statements for each status.

--- src/App.js
+++ src/App.js
@@ -1,27 +1,51 @@
 // import { Button, FormControl, InputLabel, Input } from "@mui/material";
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "./App.css";
 import Form from "./components/Form";
 import Todos from "./components/Todos";
 
 
 function App() {
-  
-  
+
+  //states
   const [todos, setTodos] = useState([]);
   const [status, setStatus] = useState("all");
+  const [filteredTodos, setFilteredTodos] = useState([]);
 
+  //useEffect 
+  useEffect(() => {
+    filterHandler();
+
+  }, [todos, status])
+
+  //functions 
+
+  const filterHandler = () => {
+    switch (status) {
+      case 'completed':
+        setFilteredTodos(todos.filter((todo) => todo.completed == true));
+        break;
+      case 'uncompleted':
+        setFilteredTodos(todos.filter((todo) => todo.completed == false));
+        break;
+      default:
+        setFilteredTodos(todos);
+        break;
+    }
+  }
 
   return (
     <div className="App">
-    <header>
-    <h1>ToDo App </h1>
-    </header>
-    
+      <header>
+        <h1>ToDo App </h1>
+      </header>
 
-      <Form todos={todos} setTodos={setTodos} setStatus={setStatus} ad/>
-      <Todos todos={todos} setTodos={setTodos}   />
-     
+      <div className="container">
+        <Form todos={todos} setTodos={setTodos} setStatus={setStatus} />
+        <Todos todos={todos} setTodos={setTodos} filteredTodos={filteredTodos} />
+      </div>
+
+
     </div>
   );
 }
